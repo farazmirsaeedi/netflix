892293927375ee5b7669d9444b5b97a80a1fcf51
farazmirsaeedi <farazmirsaeedi@gmail.com>
2024-05-28 10:29:40 -0700

    import log/sign and Account and Auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
-// import { useState } from 'react'
 import { Route, Routes } from "react-router-dom";
-
-
-import Navbar from "./Components/Navbar"
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-
+import { AuthContextProvider } from "./context/Authcontext";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Account from "./components/Account";
+import ProtectedRouter from "./components/ProtectedRouter";
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
     <>
-    <Navbar />
-    <Routes>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Signup" element={<Signup />} />
+          <Route path="/Account" element={<ProtectedRouter><Account /></ProtectedRouter>} />
         </Routes>
+      </AuthContextProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
